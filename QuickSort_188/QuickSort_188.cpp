860ff264a9daa0d35147680707d9f8dc324f1130
DeniArif <deniarifwibowo82@gmail.com>
2023-03-31 11:02:35 +0700

    comit 2

--- QuickSort_188/QuickSort_188.cpp
+++ QuickSort_188/QuickSort_188.cpp
@@ -5,3 +5,18 @@ int arr[20];
 int cmp_count = 0;
 int mov_count = 0;
 int n;
+void input() {
+	while (true)
+	{
+		cout << "masukkan panjang element array : ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "n\ Maksimum panjang array adalah 20" << endl;
+	}
+	cout << "\n------------------" << endl;
+	cout << "\nEnter array element" << endl;
+	cout << "\n------------------" << endl;
+}
